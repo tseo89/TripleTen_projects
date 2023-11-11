498f1a4f41fd591b17feaa05bd6ffa41103b5896
Tina Seo <tseo89@gmail.com>
2023-11-11 13:56:13 -0800

    Add files via upload

---
+++ 4/app.py
@@ -0,0 +1,62 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+
+df = pd.read_csv('vehicles_us.csv')
+df['manufacturer'] = df['model'].apply(lambda x: x.split()[0])
+
+st.header('Data viewer')
+show_manuf_1k_ads = st.checkbox('Include manufacturers with less than 1000 ads')
+if not show_manuf_1k_ads:
+    df = df.groupby('manufacturer').filter(lambda x: len(x) > 1000)
+
+st.dataframe(df)
+st.header('Vehicle types by manufacturer')
+st.write(px.histogram(df, x='manufacturer', color='type'))
+st.header('Histogram of `condition` vs `model_year`')
+
+# -------------------------------------------------------
+# histograms in plotly:
+# fig = go.Figure()
+# fig.add_trace(go.Histogram(x=df[df['condition']=='good']['model_year'], name='good'))
+# fig.add_trace(go.Histogram(x=df[df['condition']=='excellent']['model_year'], name='excellent'))
+# fig.update_layout(barmode='stack')
+# st.write(fig)
+# works, but too many lines of code
+# -------------------------------------------------------
+
+# histograms in plotly_express:
+st.write(px.histogram(df, x='model_year', color='condition'))
+# a lot more concise!
+# -------------------------------------------------------
+
+st.header('Compare price distribution between manufacturers')
+manufac_list = sorted(df['manufacturer'].unique())
+manufacturer_1 = st.selectbox('Select manufacturer 1',
+                              manufac_list, index=manufac_list.index('chevrolet'))
+
+manufacturer_2 = st.selectbox('Select manufacturer 2',
+                              manufac_list, index=manufac_list.index('hyundai'))
+mask_filter = (df['manufacturer'] == manufacturer_1) | (df['manufacturer'] == manufacturer_2)
+df_filtered = df[mask_filter]
+normalize = st.checkbox('Normalize histogram', value=True)
+if normalize:
+    histnorm = 'percent'
+else:
+    histnorm = None
+st.write(px.histogram(df_filtered,
+                      x='price',
+                      nbins=30,
+                      color='manufacturer',
+                      histnorm=histnorm,
+                      barmode='overlay'))
+
+
+st.header('Fuel type, Cylinders, Type affect on Price')                  
+list_for_scatter=['fuel','cylinders','type']
+choice_for_scatter=st.selectbox('Price dependency on ',list_for_scatter)
+fig2=px.scatter(df,x='price',y=choice_for_scatter,color='manufacturer',hover_data=['model_year'])
+fig2.update_layout(
+    title="<b>Price vs {}</b>".format(choice_for_scatter))
+st.plotly_chart(fig2)
